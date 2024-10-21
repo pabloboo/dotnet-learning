6bd1c4597b56178acec56e9d4161f91600bf55fd
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-10-21 11:17:09 +0200

    C# Tutorial For Beginners - Learn C# Basics

---
+++ c#/c#_fundamentals/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        byte number = 1;
+        Console.WriteLine(number);
+
+        float totalNumber = 20.95f;
+        char character = 'A';
+        string firstName = "Pablo";
+        bool isWorking = false;
+    }
+}
